8ee55a5cccfc56de6818463bc4a4cc644f0b4595
Armando García <art.gamir@gmail.com>
2023-12-19 02:56:51 -0600

    first commit

--- src/components/StreamerCard.jsx
+++ src/components/StreamerCard.jsx
@@ -4,14 +4,17 @@ export default function StreamerCard(props) {
             classnames += "NEW"
         }
     return(
-        <article >
+        <article>
             <a className = {classnames} href="#">
                 <img className="avatar"src={props.avatar}/>
                 <div className="name-wraper">
                     <h2 className="name">{props.name}</h2>
                     <p className="cat">{props.cat}</p>
                 </div>
-                {props.status === "offline" ? <p>offline</p> : <div className="viewers-container"><div className="red-dot"></div><div className="viewers-aside">{props.status}</div></div>}
+                
+                <div className="status-container">
+                {props.status === "offline" ? <p>offline</p> : <div><div className="red-dot"></div><div className="viewers-aside">{props.status}</div></div>}
+                </div>
             </a>
         </article>
     )
